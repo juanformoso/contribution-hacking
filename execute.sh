623baf8fddc4c2860d9f20f28fcb6ddd22c3f5ea
Juan Manuel Formoso <jformoso@gmail.com>
2016-06-08 12:37:45 -0300

    script

---
+++ execute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1 in 20 chance to execute
+if (( RANDOM % 20 == 0 )); then
+  JUNK=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  echo "$JUNK" > junk
+  git commit -am "$JUNK"
+  git push origin master
+fi
