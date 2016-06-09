39561ac98a1d30a19dd23872a9567e6bab91e185
Juan Manuel Formoso <jformoso@gmail.com>
2016-06-09 13:12:43 -0300

    adjusted probability again

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 1 in 30 chance to execute
-if (( RANDOM % 30 == 0 )); then
+# 1 in 20 chance to execute
+if (( RANDOM % 20 == 0 )); then
   JUNK=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   echo "$JUNK" > junk
   git commit -am "$JUNK"
