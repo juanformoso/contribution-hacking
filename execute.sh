3c38217026cd32fe1e95497deb0aa0dfc987ed65
Juan Manuel Formoso <jformoso@gmail.com>
2018-03-28 11:05:30 -0300

    etmEF69bDM7IRk4XuVkEJL9kQbEs22Rw

--- execute.sh
+++ execute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 1 in 20 chance to execute
-if (( RANDOM % 20 == 0 )); then
-  JUNK=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+#if (( RANDOM % 20 == 0 )); then
+  JUNK=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   echo "$JUNK" > junk
   git commit -am "$JUNK"
   git push origin master
-fi
+#fi
